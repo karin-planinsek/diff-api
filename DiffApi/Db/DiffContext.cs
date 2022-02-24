baacda267c624597d172fdba3a320a3ab2406046
karin-planinsek <karinp@zaslon-telecom.si>
2022-02-24 16:25:17 +0100

    Create db context and use in memory database

---
+++ DiffApi/Db/DiffContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DiffApi.Models;
+
+namespace DiffApi.Db
+{
+    public class DiffContext : DbContext
+    {
+        public DiffContext(DbContextOptions<DiffContext> options) : base(options) { }
+
+        public DbSet<LeftDiff> LeftDiffs { get; set; }
+        public DbSet<RightDiff> RightDiffs { get; set; }
+    }
+}
