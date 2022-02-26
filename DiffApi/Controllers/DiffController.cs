4d6063463ceeae8006926785f2f4925be4397c9b
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2022-02-26 19:36:22 +0100

    Create endpoint for creating and updating left diffs

--- DiffApi/Controllers/DiffController.cs
+++ DiffApi/Controllers/DiffController.cs
@@ -1,22 +1,39 @@
+using DiffApi.Dto;
+using DiffApi.Infrastructure.Services;
+using DiffApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DiffApi.Controllers
 {
     [ApiController]
-    [Route("v1/[controller]")]
+    [Route("v1/diff")]
     public class DiffController : ControllerBase
     {
         private readonly ILogger<DiffController> _logger;
+        private readonly ILeftDiffService _leftDiffService;
 
-        public DiffController(ILogger<DiffController> logger)
+        public DiffController(ILogger<DiffController> logger, ILeftDiffService leftDiffService)
         {
             _logger = logger;
+            _leftDiffService = leftDiffService;
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get()
+        [HttpPut("{id}/left")]
+        public IActionResult CreateLeftDiff(int id, [FromBody] DiffRequestDto data)
         {
-            
+            if (data != null)
+            {
+                LeftDiff leftDiff = new LeftDiff { Id = id, Data = data.Data };
+                var diffId = _leftDiffService.FindDiffById(id);
+
+                if (diffId == null) _leftDiffService.CreateDiff(leftDiff);
+                else _leftDiffService.UpdateDiff(leftDiff);
+
+                return Ok();
+            } else
+            {
+                return BadRequest();
+            }
         }
     }
 }
