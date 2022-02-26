312ef3f32777c89208d76d0b9b2b48bc1471d81c
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2022-02-26 21:52:47 +0100

    Create RightDiffService

---
+++ DiffApi/Infrastructure/Services/IRightDiffService.cs
@@ -0,0 +1,11 @@
+﻿using DiffApi.Models;
+
+namespace DiffApi.Infrastructure.Services
+{
+    public interface IRightDiffService
+    {
+        void CreateDiff(RightDiff diff);
+        void UpdateDiff(RightDiff diff);
+        RightDiff FindDiffById(int id);
+    }
+}
---
+++ DiffApi/Infrastructure/Services/RightDiffService.cs
@@ -0,0 +1,31 @@
+﻿using DiffApi.Infrastructure.Repositories;
+using DiffApi.Models;
+
+namespace DiffApi.Infrastructure.Services
+{
+    public class RightDiffService : IRightDiffService
+    {
+        private readonly IRightDiffRepository _rightDiffRepository;
+
+        public RightDiffService(IRightDiffRepository rightDiffRepository)
+        {
+            _rightDiffRepository = rightDiffRepository;
+        }
+        public void CreateDiff(RightDiff diff)
+        {
+            _rightDiffRepository.CreateRightDiff(diff);
+        }
+
+        public RightDiff FindDiffById(int id)
+        {
+            var rightDiff = _rightDiffRepository.GetRightDiff(id);
+
+            return rightDiff;
+        }
+
+        public void UpdateDiff(RightDiff diff)
+        {
+            _rightDiffRepository.UpdateRightDiff(diff);
+        }
+    }
+}
