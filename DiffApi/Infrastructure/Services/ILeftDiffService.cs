2b731dc60988cc3105df633139e4e890e03f003e
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2022-02-26 19:20:03 +0100

    Create LeftDiffService

---
+++ DiffApi/Infrastructure/Services/ILeftDiffService.cs
@@ -0,0 +1,11 @@
+﻿using DiffApi.Models;
+
+namespace DiffApi.Infrastructure.Services
+{
+    public interface ILeftDiffService
+    {
+        int CreateDiff(LeftDiff diff);
+        int UpdateDiff(LeftDiff diff);
+        LeftDiff FindDiffById(int id);
+    }
+}
---
+++ DiffApi/Infrastructure/Services/LeftDiffService.cs
@@ -0,0 +1,35 @@
+﻿using DiffApi.Infrastructure.Repositories;
+using DiffApi.Models;
+
+namespace DiffApi.Infrastructure.Services
+{
+    public class LeftDiffService : ILeftDiffService
+    {
+        private readonly ILeftDiffRepository _leftDiffRepository;
+
+        public LeftDiffService(ILeftDiffRepository leftDiffRepository)
+        {
+            _leftDiffRepository = leftDiffRepository;
+        }
+        public int CreateDiff(LeftDiff diff)
+        {
+            var leftDiff = _leftDiffRepository.CreateLeftDiff(diff);
+
+            return leftDiff.Id;
+        }
+
+        public LeftDiff FindDiffById(int id)
+        {
+            var leftDiff = _leftDiffRepository.GetLeftDiff(id);
+
+            return leftDiff;
+        }
+
+        public int UpdateDiff(LeftDiff diff)
+        {
+            var leftDiff = _leftDiffRepository.UpdateLeftDiff(diff);
+
+            return leftDiff.Id;
+        }
+    }
+}
