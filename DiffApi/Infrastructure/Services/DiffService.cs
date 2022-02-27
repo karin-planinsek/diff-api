c9352c1cac8260a1dc303a929a3a296dc114295a
KARIN.PLANINSEK <karin.planinsek@g-users.com>
2022-02-27 13:57:35 +0100

    Create service for calculating diff offset and length

---
+++ DiffApi/Infrastructure/Services/DiffService.cs
@@ -0,0 +1,43 @@
+﻿using DiffApi.Dto;
+using System.Text;
+
+namespace DiffApi.Infrastructure.Services
+{
+    public class DiffService : IDiffService
+    {
+        public DiffResponseDto GetDiff(byte[] leftDiff, byte[] rightDiff)
+        {
+            if (leftDiff.SequenceEqual(rightDiff)) 
+                return new DiffResponseDto { DiffResultType = Response.Equals };
+            else if (leftDiff.Length != rightDiff.Length) 
+                return new DiffResponseDto { DiffResultType = Response.SizeDoNotMatch };
+            else 
+                return new DiffResponseDto { DiffResultType = Response.ContentDoNotMatch, Diffs = GetDiffDetails(leftDiff, rightDiff) };
+        }
+
+        // find diffs and calculate offsets and lengths
+        public IEnumerable<DiffDto> GetDiffDetails(byte[] leftDiff, byte[] rightDiff)
+        {
+            List<DiffDto> diffs = new List<DiffDto>();
+            var diffLength = 0;
+
+            for (int i = 0; i < leftDiff.Length; i++)
+            {
+                if (leftDiff[i] == rightDiff[i])
+                {
+                    if (diffLength > 0)
+                    {
+                        diffs.Add(new DiffDto { Length = diffLength, Offset = i - diffLength });
+                        diffLength = 0;
+                    }
+                }
+                else diffLength++;
+            }
+
+            if (diffLength > 0)
+                diffs.Add(new DiffDto { Length = diffLength, Offset = leftDiff.Length - diffLength });
+
+            return diffs;
+        }
+    }
+}
---
+++ DiffApi/Infrastructure/Services/IDiffService.cs
@@ -0,0 +1,9 @@
+﻿using DiffApi.Dto;
+
+namespace DiffApi.Infrastructure.Services
+{
+    public interface IDiffService
+    {
+        DiffResponseDto GetDiff(byte[] leftDiff, byte[] rightDiff);
+    }
+}
